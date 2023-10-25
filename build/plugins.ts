27b43b96bacc016993028bff2f2b58e0d19927e5
Q <lqandmore@163.com>
2023-10-25 22:27:29 +0800

    自定义插件

---
+++ build/plugins.ts
@@ -0,0 +1,44 @@
+import fs from "fs-extra"
+import createVuePlugin from "@vitejs/plugin-vue";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import path from "path";
+import marked from "marked"
+
+const fileRegex = /\.(md)$/;
+const vuePlugin = createVuePlugin({ include: [/\vue$/, /\.md$/] });
+function demoLoader(code, path) {
+  const token = marked.lexer(code)
+  return vueComponent
+}
+
+function transfromMD() {
+  return {
+    name: 'vite-plugin-md',
+    async transfrom(_, path) {
+      const code = await fs.readFile(path, 'utf-8')
+      if (path.endsWith('.md')) {
+        return demoLoader(code, path)
+      }
+    },
+    async handleHotUpdate(ctx) {
+      const { file } = ctx
+      if (fileRegex.test(file)) {
+        const code = await fs.readFile(file, 'utf-8')
+        let codeLoader
+        if (path.extname() === '.md') {
+          codeLoader = demoLoader(code, path)
+        }
+        return vuePlugin.handleHotUpdate({
+          ...ctx,
+          read: ()=> codeLoader
+        })
+      }
+    }
+  }
+}
+
+
+export function getPluginsList(commond: string, VITE_CDN: boolean) {
+  return [vue(), vueJsx(),];
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import { getPluginsList } from '@build/plugins'
+
+
 
 const pathResolve = (dir: string): string => {
   return resolve(__dirname, ".", dir)
@@ -10,11 +13,13 @@ const alias: Record<string, string> = {
   "@": pathResolve("src"),
   "@build": pathResolve("build")
 }
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias
   },
-  plugins: [vue()],
-  
+  plugins: getPluginsList("build", false),
+
 })
